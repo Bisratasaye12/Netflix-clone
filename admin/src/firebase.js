9bb388590b8889ebd310993c2dc12757d460c057
Bisrat Asaye <138536048+Bisratasaye12@users.noreply.github.com>
2024-12-12 01:39:21 +0300

    Update firebase.js

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD7hJ4j5q4oSZ_Rfd61z8plpkiSF0mKtfA",
-    authDomain: "netflix-868e8.firebaseapp.com",
-    projectId: "netflix-868e8",
-    storageBucket: "netflix-868e8.appspot.com",
-    messagingSenderId: "949617975867",
-    appId: "1:949617975867:web:b8cf850a390ff2aac2321e",
-    measurementId: "G-S6E4NPMBTB"
+    apiKey: "",
+    authDomain:"",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
